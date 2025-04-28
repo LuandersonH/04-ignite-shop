95c090cd297035b5402d5202df31d178e9871fee
LuandersonH <work.luanderson@gmail.com>
2025-04-28 03:36:41 -0300

    Trabalhando com SEO

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -16,7 +16,7 @@ export default async function handler(
     return res.status(400).json({ error: "Price not found." });
   }
 
-  const sucessURL = `${process.env.NEXT_URL}/success`;
+  const sucessURL = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`;
   const cancelURL = `${process.env.NEXT_URL}`;
 
   const checkoutSession = await stripe.checkout.sessions.create({
--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,19 +1,67 @@
 import Link from "next/link";
 import { ImageContainer, SuccessContainer } from "../styles/pages/sucess";
+import { GetServerSideProps } from "next";
+import { stripe } from "../lib/stripe";
+import Stripe from "stripe";
+import Image from "next/image";
 
-export default function Success() {
+interface SucessProps {
+  customerName: string;
+  product: {
+    name: string;
+    imageUrl: string;
+  };
+}
+
+export default function Success({ customerName, product }: SucessProps) {
   return (
     <SuccessContainer>
       <h1>Compra efetuada!</h1>
 
-      <ImageContainer></ImageContainer>
+      <ImageContainer>
+        <Image src={product.imageUrl} width={120} height={110} alt=""></Image>
+      </ImageContainer>
 
       <p>
-        Uhuul <strong>Luanderson</strong>, sua <strong>Camiseta Tal</strong> já
-        está a caminha da sua casa.
+        Uhuul <strong>{customerName}</strong>, sua{" "}
+        <strong>{product.name}</strong> já está a caminha da sua casa.
       </p>
 
       <Link href="/">Voltar ao catálogo</Link>
     </SuccessContainer>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  if (!query.session_id) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  const sessionId = String(query.session_id);
+
+  const session = await stripe.checkout.sessions.retrieve(sessionId, {
+    expand: ["line_items", "line_items.data.price.product"],
+  });
+
+  const customerName = session.customer_details?.name;
+
+  const product = session.line_items?.data[0].price?.product as Stripe.Product;
+
+  console.log(customerName);
+  console.log(product);
+
+  return {
+    props: {
+      customerName,
+      product: {
+        name: product.name,
+        imageUrl: product.images[0],
+      },
+    },
+  };
+};
